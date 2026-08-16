82a6d9a353a2275f7375db8284da764189ab9043
Marcio Lucas <marciolucascmatos@gmail.com>
2026-08-15 21:34:30 -0300

    Sistema de validação e armazenamento de dados, primeira parte

--- Sistema_cadastro.py
+++ Sistema_cadastro.py
@@ -1,13 +1,33 @@
 import tkinter
 
+from Validador import usuario
+
 janela_principal = tkinter.Tk()
 janela_principal.geometry("1000x600")
 janela_principal.title("Cadastro")
 janela_principal.maxsize(1000, 600)
 janela_principal.minsize(1000, 600)
 
-Frame_inserção = tkinter.Frame(janela_principal, bg = "gray", bd = 2, relief= "solid", width = 300, height = 400)
+Frame_inserção = tkinter.Frame(janela_principal, bg = "gray", bd = 2, relief= "solid",width=300,  height = 400)
 Frame_inserção.place(x = 350, y = 100)
+
+titulo = tkinter.Label(Frame_inserção, font=("Arial", 20, "bold"), text="Cadastro")
+titulo.place(x = 85, y = 30)
+
+email_label = tkinter.Label(Frame_inserção, font=("Arial", 12, "bold"), text="Email" )
+email_label.place(x = 20, y = 100)
+
+senha_label = tkinter.Label(Frame_inserção, font=("Arial", 12, "bold"), text="Senha" )
+senha_label.place(x = 20, y = 200)
+
+email_entry = tkinter.Entry(Frame_inserção, font=("Arial", 12))
+email_entry.place(x = 20, y = 150, height=35, width=250)
+
+senha_entry = tkinter.Entry(Frame_inserção, font=("Arial", 12))
+senha_entry.place(x = 20, y = 250, height=35, width=250)
+
+enviar = tkinter.Button(Frame_inserção, text="Enviar", font=("Arial", 20, "bold"), command=usuario.validar)
+enviar.place(height=50, width=285, x= 5, y= 340)
 
 janela_principal.mainloop()
 
--- Validador.py
+++ Validador.py
@@ -1,4 +1,23 @@
+from tkinter import messagebox
+
+from Sistema_cadastro import email_entry, senha_entry
+
+senha = senha_entry.get()
+email = email_entry.get()
+
 class usuario:
     def __init__(self,email, senha):
         self.email = email
-        self.senha = senha
+        self.senha = senha   
+    def validar():
+          global  senha, email
+          if len(senha) < 8:
+              messagebox.showerror("Erro", "Sua senha deve ter 8 caracteres")
+          else:
+              pass
+          
+          if "@" not in email:
+              messagebox.showerror("Erro", "Email inválido")
+          else:
+              pass
+        
