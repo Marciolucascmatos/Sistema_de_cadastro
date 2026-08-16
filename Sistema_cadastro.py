d3c142c49b4b42ba9643b323e8d54528e0e5b115
Marcio Lucas <marciolucascmatos@gmail.com>
2026-08-16 10:04:45 -0300

    armazenamento de dados

--- Sistema_cadastro.py
+++ Sistema_cadastro.py
@@ -29,6 +29,8 @@ senha_entry.place(x = 20, y = 250, height=35, width=250)
 def armzn():
     usuario_fic = usuario(email_entry.get(), senha_entry.get())
     usuario_fic.validar()
+    email_entry.delete(0, "end")
+    senha_entry.delete(0, "end")
     
 enviar = tkinter.Button(Frame_inserção, text="Enviar", font=("Arial", 20, "bold"), command=armzn)
 enviar.place(height=50, width=285, x= 5, y= 340)
